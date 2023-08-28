0d2e30a184ee8c1d2a5f701b7aa5941886ec9ce5
Sankalp2226 <140904094+Sankalp2226@users.noreply.github.com>
2023-08-28 20:09:59 +0530

    Add files via upload

---
+++ TS.js
@@ -0,0 +1,13 @@
+const convertBtn = document.getElementById("convertBtn");
+const celsiusInput = document.getElementById("celsius");
+const resultParagraph = document.getElementById("result");
+
+convertBtn.addEventListener("click", () => {
+  const celsius = parseFloat(celsiusInput.value);
+  if (!isNaN(celsius)) {
+    const fahrenheit = (celsius * 9/5) + 32;
+    resultParagraph.textContent = `${celsius.toFixed(2)}°C is equal to ${fahrenheit.toFixed(2)}°F`;
+  } else {
+    resultParagraph.textContent = "Please enter a valid temperature in Celsius.";
+  }
+});
